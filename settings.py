45dc5f025810c02eb278dc3a9555601304d5ec86
evg-cv <evgenijkolotov333@gmail.com>
2020-12-20 11:32:21 -0300

    Complete

--- settings.py
+++ settings.py
@@ -10,5 +10,5 @@ SENT_CLASSIFIER_MODEL_PATH = os.path.join(CUR_DIR, 'utils', 'model', 'sent_class
 PERTINENT_MODEL_PATH = os.path.join(CUR_DIR, 'utils', 'model', 'pertinent.pkl')
 WORD_MODEL_PATH = os.path.join(CUR_DIR, 'utils', 'model', 'pruned.word2vec.txt')
 
-TRAINING_DATA_PATH = "/media/main/Data/Task/ArticleAnalysis/training_data.xlsx"
-INPUT_EXCEL_PATH = "/media/main/Data/Task/ArticleAnalysis/input_file.xlsx"
+TRAINING_DATA_PATH = ""
+INPUT_EXCEL_PATH = ""
